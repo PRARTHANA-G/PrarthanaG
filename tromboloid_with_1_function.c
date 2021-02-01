ac2655529ef308ba44c8fc0be079c38397b25197
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-01 18:00:30 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -3,10 +3,10 @@
 int main()
 {
 		
-float volume,h,b,f,d;
-printf("enter values of h,b,d respectively");
-scanf("%f%f%f",&h ,&b,&d);
-volume=((h*b*d)+(d/b))/3;
-printf("volume is:%f",volume);
-return 0;
+	float volume,h,b,f,d;
+	printf("enter values of h,b,d respectively");
+	scanf("%f%f%f",&h ,&b,&d);
+	volume=((h*b*d)+(d/b))/3;
+	printf("volume is:%f",volume);
+	return 0;
 }
