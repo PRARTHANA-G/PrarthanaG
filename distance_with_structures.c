0fdf609594d766c6a8d18e60234ec824c69849d5
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-08 10:25:20 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,22 +1,38 @@
 //WAP to find the distance between two points using structures and 4 functions.
 #include<stdio.h>
 #include<math.h>
-float clac(struct,struct);
+#include<stdlib.h>
+
 struct cor
 {
 	float x,y;
-}
+};
+struct l{
+    struct cor begin;
+    struct cor last;
+};
+float calc(struct l);
+
 int main()
 {
-	struct cor p1,p2;
-	printf("enter co-ordinates of first point p1");
-	scanf("%f%f",&p1.x,&p1.y);
-	printf("enter co-ordinates of second point p2");
-	scanf("%f%f",&p2.x,&p2.y);
-	calc( p1, p2);
-}
-float calc(struct cor p1,struct cor p2)
-{
-	float dist=sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+	struct cor p;
+	struct l points;
+	float dist;
+	printf("enter x co-ordinate of point 1");
+	scanf("%f",&points.begin.x);
+	printf("enter y co-ordinate of point 1");
+	scanf("%f",&points.begin.y);
+	printf("enter x co-ordinate of point 2");
+	scanf("%f",&points.last.x);
+	printf("enter y co-ordinate of point 2");
+	scanf("%f",&points.last.y);
+	dist= calc(points);
 	printf("distance is %f",dist);
+	return 0;
+	//calc( p1, p2);
+}
+float calc(struct l points)
+{
+    float d=sqrt((points.begin.x-points.last.x)*(points.begin.x-points.last.x)+(points.begin.y-points.last.y)*(points.begin.y-points.last.y));
+    return d;
 }
