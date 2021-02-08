1c941fa1b12e5866383d81cdc70c5979e3348839
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-08 20:25:50 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -10,9 +10,9 @@ typedef struct fraction
 int main()
 {
 	int n1,d1,n2,d2;
-	printf("enter numerator and denominator of first fraction in that order");
+	printf("enter numerator and denominator of first fraction in that order\n");
 	scanf("%d%d",&n1,&d1);
-	printf("enter numerator and denominator of fraction 2");
+	printf("enter numerator and denominator of fraction two\n");
 	scanf("%d%d",&n2,&d2);
 	fraction f1={n1,d1};
 	fraction f2={n2,d2};
@@ -20,6 +20,14 @@ int main()
 }
 void res(fraction f1,fraction f2)
 {
-fraction r={f1.numerator*f2.denominator+f2.numerator+f1.denominator,f1.denominator*f2.denominator};
+if(f1.denominator==f2.denominator){
+    fraction r={f1.numerator+f2.numerator,f1.denominator};
+    printf("result is %d/%d",r.numerator,r.denominator);
+}
+else
+{
+fraction r={f1.numerator*f2.denominator+f2.numerator*f1.denominator,f1.denominator*f2.denominator};
 printf("result is %d/%d",r.numerator,r.denominator);
 }
+}
+
