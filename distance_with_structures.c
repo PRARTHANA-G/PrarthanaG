f39e940575ca8881b6512528c787d85b3a37bce6
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-09 09:58:02 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -12,27 +12,43 @@ struct l{
     struct cor last;
 };
 float calc(struct l);
+int xinput();
+int yinput();
 
 int main()
 {
-	struct cor p;
+     struct cor p;
 	struct l points;
 	float dist;
-	printf("enter x co-ordinate of point 1");
-	scanf("%f",&points.begin.x);
-	printf("enter y co-ordinate of point 1");
-	scanf("%f",&points.begin.y);
-	printf("enter x co-ordinate of point 2");
-	scanf("%f",&points.last.x);
-	printf("enter y co-ordinate of point 2");
-	scanf("%f",&points.last.y);
+	input1();
+	input2();
 	dist= calc(points);
 	printf("distance is %f",dist);
 	return 0;
 	//calc( p1, p2);
 }
+int input1()
+{
+    struct cor p;
+	struct l points;
+    printf("enter x co-ordinate of point 1");
+	scanf("%f",&points.begin.x);
+	printf("enter y co-ordinate of point 1");
+	scanf("%f",&points.begin.y);
+    
+}
+int input2()
+{
+    struct cor p;
+	struct l points;
+    printf("enter x co-ordinate of point 2");
+	scanf("%f",&points.last.x);
+	printf("enter y co-ordinate of point 2");
+	scanf("%f",&points.last.y);
+}
 float calc(struct l points)
 {
     float d=sqrt((points.begin.x-points.last.x)*(points.begin.x-points.last.x)+(points.begin.y-points.last.y)*(points.begin.y-points.last.y));
-    return d;
+	return d;
+    
 }
