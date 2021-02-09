980b6909f60dc1dae2ec0d00c2b83e2e73bb7a56
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-09 12:37:24 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,34 +1,51 @@
-//WAP to find the sum of n fractions.
-#include<stdio.h>
-struct frac{
-int n;
-int d;
-}
-void print(struct frac b)
-{
-	struct frac r;
-	r.n=b.n*b.d+b.n*b.d;
-	r.d=b.d*b.d;
-	printf("%d/%d",r.n,r.d);
-}
+#include <stdio.h>
+struct fraction{
+    int numerator[100];
+    int denominator[100];
+};
 int main()
 {
-int i,num;
-printf("enter number of fractions");
-scanf("%d",&num);
-int a[num],b[];
-for(i=0;i<num;i++)
-{
-printf("enter %d fraction numerator",i+1);
-scanf("%d",&a[i]);
-printf("enter %d fraction denominator",i+1);
-scanf("%d",&b[i]);
+    int n;
+    printf("Enter number of fractions: ");
+    scanf("%d", &n);
+    int numer[n],denom[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter numerator of %d fraction", (i+1));
+        scanf("%d", &numer[i]);
+        printf("Enter denominator of %d fraction", (i+1));
+        scanf("%d", &denom[i]);
+    }
+    redform(n, numer, denom);
+    return 0;
 }
-int lcm()
+void redform(int n, int numer[],int denom[])
 {
-int x=ar[0];
-for(i=0;i<n;i++)
+    int fnum = 0;
+    int fden = lcm(denom, n);
+    for (int i = 0; i < n; i++) {
+        fnum = fnum+ (numer[i]) * (fden/ denom[i]);
+    }
+    int GCD = commond(fnum,fden);
+ 
+    fnum =fnum/ GCD;
+    fden = fden/GCD;
+    printf("The answer is %d/%d", fnum,fden);
+}
+int lcm(int array[], int n)
 {
-x=ar[i]*a[i+1];
-return x;
+    int x = array[0];
+    for (int i = 1; i < n; i++) {
+        x = ((array[i] * x)/ commond(array[i], x));
+    }
+    return x;
+}
+int commond(int a, int b)
+{
+    if (b == 0) 
+    {
+        return a;
+    }
+ 
+    return commond(b, a % b);
 }
