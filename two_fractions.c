bfa49299322f5969c63ed1eb53142f99ac304c0d
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-20 08:04:37 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,33 +1,44 @@
 //WAP to find the sum of two fractions.
 #include<stdio.h>
 #include<stdlib.h>
-//int res(int,int);
 typedef struct fraction
 {
-	int numerator;
-	int denominator;
+	int num;
+	int denom;
 }fraction;
+fraction input1()
+{
+    
+    struct fraction f1;
+	printf("enter numerator and denominator of first fraction in that order\n");
+	scanf("%d%d",&f1.num,&f1.denom);
+
+	return f1;
+}
+fraction input2()
+{
+    //int n2,d2;
+    struct fraction f2;
+    printf("enter numerator and denominator of fraction two\n");
+	scanf("%d%d",&f2.num,&f2.denom);
+	//f2={n2,d2};
+	return f2;
+}
 int main()
 {
-	int n1,d1,n2,d2;
-	printf("enter numerator and denominator of first fraction in that order\n");
-	scanf("%d%d",&n1,&d1);
-	printf("enter numerator and denominator of fraction two\n");
-	scanf("%d%d",&n2,&d2);
-	fraction f1={n1,d1};
-	fraction f2={n2,d2};
+	
+struct fraction f1,f2;
+f1=input1();
+f2=input2();
+	
 	res(f1,f2);
 }
 void res(fraction f1,fraction f2)
 {
-if(f1.denominator==f2.denominator){
-    fraction r={f1.numerator+f2.numerator,f1.denominator};
-    printf("result is %d/%d",r.numerator,r.denominator);
+if(f1.denom==f2.denom){
+    fraction r={f1.num+f2.num,f1.denom};
+    printf("result is %d/%d",r.num,r.denom);
 }
-else
-{
-fraction r={f1.numerator*f2.denominator+f2.numerator*f1.denominator,f1.denominator*f2.denominator};
-printf("result is %d/%d",r.numerator,r.denominator);
-}
+
 }
 
