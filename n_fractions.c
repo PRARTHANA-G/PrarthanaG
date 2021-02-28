6fd181ac438f8393c460aa355d7e4e83fa05af35
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-02-28 14:35:39 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,51 +1,66 @@
-#include <stdio.h>
-struct frac{
-    int numerator[90];
-    int denominator[90];
-}f;
-int main()
+#include<stdio.h>
+typedef struct fraction 
 {
-    int n;
-    printf("Enter number of fractions: ");
-    scanf("%d", &n);
-    int numer[n],denom[n];
+    int num;
+    int denom;
+}f;
+int gcd(int a,int b)
+{
+     int hcf;
+  for (int i = 2; i <= a && i <= b; i++)
+    {
+
+      if (a % i == 0 && b % i == 0)
+	hcf = i;
+    }
+  return hcf;
+}
+f input(int n)
+{
+    //int n;
+    struct fraction f[n];
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("Enter value of %d fraction",i+1);
+        scanf("%d%d",&f[i].num,&f[i].denom);
+        
+    }
+    return f[n];
+    
+}
+f add(struct fraction f[],int n)
+{
+     struct fraction sum;
+     int hcf;
     for(int i=0;i<n;i++)
     {
-        printf("Enter numerator of %d fraction", (i+1));
-        scanf("%d", &f.numerator[i]);
-        printf("Enter denominator of %d fraction", (i+1));
-        scanf("%d", &f.denominator[i]);
+    
+       struct fraction sum={((f[i].num*f[i+1].denom)+(f[i+1].num*f[i].denom)),(f[i].num*f[i+1].denom)};
     }
-    form(n, f.numerator, f.denominator);
-    return 0;
+     hcf=gcd(sum.num,sum.denom);
+     //sum={((f[i].num*f[i+1].denom)+(f[i+1].num*f[i].denom))/hcf,(f[i].num*f[i+1].denom)/hcf};
+     struct fraction result={sum.num/hcf,sum.denom/hcf};
+    
+    return result;
 }
-void form(int n, int numer[],int denom[])
+
+void print(struct fraction sum)
 {
-    int fnum = 0;
-    int fden = lcm(denom, n);
-    for (int i = 0; i < n; i++) {
-        fnum = fnum+ (numer[i]) * (fden/ denom[i]);
-    }
-    int GCD = commond(fnum,fden);
- 
-    fnum =fnum/ GCD;
-    fden = fden/GCD;
-    printf("The answer is %d/%d", fnum,fden);
+    printf("sum is %d/%d",sum.num,sum.denom);
 }
-int lcm(int array[], int n)
+int main()
 {
-    int x = array[0];
-    for (int i = 1; i < n; i++) {
-        x = ((array[i] * x)/ commond(array[i], x));
-    }
-    return x;
+    int n,a;
+    struct fraction f[n];
+    struct fraction sum;
+    printf("Enter value of n:");
+    scanf("%d",&n);
+    f[n]=input(n);
+    sum=add(f,n);
+    print(sum);
 }
-int commond(int a, int b)
-{
-    if (b == 0) 
-    {
-        return a;
-    }
- 
-    return commond(b, a % b);
-}
+    
+
+
+
