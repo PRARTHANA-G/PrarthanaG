689cf25815876b8c976b066dfdb2ad5f170d9957
PRARTHANA-G <71335999+PRARTHANA-G@users.noreply.github.com>
2021-03-18 22:08:07 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,66 +1,80 @@
 #include<stdio.h>
-typedef struct fraction 
+struct fraction
 {
-    int num;
-    int denom;
-}f;
-int gcd(int a,int b)
+    int n;
+    int d;
+};
+struct fraction one_input()
 {
-     int hcf;
-  for (int i = 2; i <= a && i <= b; i++)
+    struct fraction i;
+    printf("Enter fraction- Numerator followed by denominator:\n");
+    scanf("%d%d",&i.n,&i.d);
+    return i;
+}
+struct fraction n_input(int n,struct fraction a[n])
+{
+    //struct fraction a[n];
+    for(int i=0;i<n;i++)
+    {
+        a[i]=one_input();
+    }
+}
+int commondovisor(int a,int b)
+{
+    int hcf=1,temp;
+    if ( a<b) 
+    {
+      temp=a;
+      a=b;
+      b=temp;
+    }
+    if ( a%b==0) 
+     return b;
+
+    for (int i = 2;i <= b/2; i++)
     {
 
       if (a % i == 0 && b % i == 0)
-	hcf = i;
+	        hcf = i;
     }
   return hcf;
+
 }
-f input(int n)
+struct fraction one_compute(struct fraction f1,struct fraction f2)
 {
-    //int n;
-    struct fraction f[n];
-    int i;
-    for(i=0;i<n;i++)
-    {
-        printf("Enter value of %d fraction",i+1);
-        scanf("%d%d",&f[i].num,&f[i].denom);
-        
-    }
-    return f[n];
-    
+    struct fraction f;
+    f.n=(f1.n*f2.d)+(f2.n*f1.d);
+    f.d=f1.d*f2.d;
+    //struct fraction f={(f1.n*f2.d+f2.n*f1.d), 
+    //(f1.d*f2.d)};
+    return f;
 }
-f add(struct fraction f[],int n)
+struct fraction n_compute(int n,struct fraction a[n])
 {
-     struct fraction sum;
-     int hcf;
+    struct fraction sum;
+    sum.n=0;//sum is initialized to 0.
+    sum.d=1;
     for(int i=0;i<n;i++)
     {
-    
-       struct fraction sum={((f[i].num*f[i+1].denom)+(f[i+1].num*f[i].denom)),(f[i].num*f[i+1].denom)};
+        sum=one_compute(a[i],sum);
     }
-     hcf=gcd(sum.num,sum.denom);
-     //sum={((f[i].num*f[i+1].denom)+(f[i+1].num*f[i].denom))/hcf,(f[i].num*f[i+1].denom)/hcf};
-     struct fraction result={sum.num/hcf,sum.denom/hcf};
-    
+    int divisor=commondovisor(sum.n,sum.d);
+    struct fraction result={sum.n/divisor,sum.d/divisor};
     return result;
 }
-
-void print(struct fraction sum)
+struct fraction print(struct fraction result)
 {
-    printf("sum is %d/%d",sum.num,sum.denom);
+    printf("The sum is:\n%d/%d",result.n,result.d);
+    
 }
 int main()
 {
-    int n,a;
-    struct fraction f[n];
-    struct fraction sum;
-    printf("Enter value of n:");
+    int n;
+    printf("Enter number of fractions\n");
     scanf("%d",&n);
-    f[n]=input(n);
-    sum=add(f,n);
-    print(sum);
-}
+    struct fraction a[n],add;
+    n_input(n,a);
+    add=n_compute(n,a);
+    print(add);
     
-
-
-
+}
